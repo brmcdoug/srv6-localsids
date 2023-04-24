8b5216946be9d0326e4ef2637949dae6fbe80896
brmcdoug <brmcdoug@cisco.com>
2023-04-24 12:58:07 -0700

    local

--- pm-kafka.py
+++ pm-kafka.py
@@ -16,28 +16,22 @@ consumer = KafkaConsumer(
 
 print('List of topics: ', consumer.topics())
 
-# for loop subscribes to Kafka messages and uploads docs to DB
+# for loop subscribes to Kafka messages and creates DB key
 print("starting loop")
 for message in consumer:
     consumer.commit() 
     message = message.value
     msgobj = json.dumps(message, indent=4)
-    print(msgobj)
-    # beware, regex'ing follows
     msg = msgobj.replace("/", "_" )
-    # msg = (re.sub("sid_context_key_u_dt4_u_dt_base_ctx_table_id", "table_id", msg))
-    # msg = (re.sub("sid_context_key_u_dt6_u_dt_base_ctx_table_id", "table_id", msg))
-    
+    print(msg)
+
     # convert json string to dict
     msgdict = json.loads(msg)
     interface = msgdict['tags']['interface_name']
     node_name = msgdict['tags']['source']
-    #print(name, sid)
 
-    # generate DB ID and Key
+    # generate DB Key
     key = node_name + "_" + interface
-    #id = "srv6_local_sids/" + key
-    msgdict['_key'] = key
     print("key", key)
     
 
